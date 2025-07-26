1defc32411c36475f6ae745fbfa7fa46a16595a9
Rahat Kabir Ifty Mac <iftyrahatkabir@gmail.com>
2025-07-27 01:44:38 +0600

    Preorder Binary Tree

---
+++ 2_preorder_traversal.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+class Node
+{
+    public:
+        int val;
+        Node* left;
+        Node* right; 
+    
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL; 
+    }
+};
+
+void preorder(Node* root)
+{
+    if(root == NULL)
+        return;
+
+    cout << root->val << " ";
+    preorder(root->left);
+    preorder(root->right);
+
+}
+
+int main()
+{
+    Node* root = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* d = new Node(50);
+    Node* e = new Node(60);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    b->left = d;
+    b->right = e;
+
+    preorder(root);
+
+    return 0;
+}
