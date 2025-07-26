21427e4cb78bdcab221e55f6a07b3ea62edb82bc
Rahat Kabir Ifty Mac <iftyrahatkabir@gmail.com>
2025-07-27 01:18:07 +0600

    Binary tree

---
+++ 1_creating_nodes.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+class Node
+{
+    public:
+        int val;
+        Node* left;
+        Node* right; 
+    
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL; 
+    }
+};
+
+int main()
+{
+    Node* root = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* d = new Node(50);
+    Node* e = new Node(60);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    b->left = d;
+    b->right = e;
+
+
+
+    return 0;
+}
